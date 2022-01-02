6c17f1215405c4e0232f9bedc6858b11626dd5e5
zyc <645251181@qq.com>
2022-01-02 17:55:48 +0800

    2022-1-2-3

--- Scrapy_Tutorial/scrapyitempipelinedemo/scrapyitempipelinedemo/settings.py
+++ Scrapy_Tutorial/scrapyitempipelinedemo/scrapyitempipelinedemo/settings.py
@@ -11,7 +11,6 @@ BOT_NAME = 'scrapyitempipelinedemo'
 
 SPIDER_MODULES = ['scrapyitempipelinedemo.spiders']
 NEWSPIDER_MODULE = 'scrapyitempipelinedemo.spiders'
-IMAGE_STORE = './images'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'scrapyitempipelinedemo (+http://www.yourdomain.com)'
@@ -25,7 +24,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -67,7 +66,7 @@ ITEM_PIPELINES = {
     'scrapyitempipelinedemo.pipelines.MongoDBPipeline': 301,
     # 'scrapyitempipelinedemo.pipelines.ElasticsearchPipeline': 302
 }
-
+IMAGES_STORE = './images'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
